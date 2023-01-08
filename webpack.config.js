f665b17b0c50a828194a9fea41a799e7dcdade81
Amruthaaaaa <92784025+Amruthaaaaa@users.noreply.github.com>
2023-01-08 17:33:23 +0530

    Setup Test dataabse environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
+
 module.exports = (env) => {
-const isProduction = env === 'production';
-const CSSExtract = new  ExtractTextPlugin('styles.css');
-  
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
   return {
     entry: './src/app.js',
     output: {
@@ -19,13 +29,13 @@ const CSSExtract = new  ExtractTextPlugin('styles.css');
       }, {
         test: /\.s?css$/,
         use: CSSExtract.extract({
-          use:[
-           {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-           } , 
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
             {
               loader: 'sass-loader',
               options: {
@@ -37,13 +47,23 @@ const CSSExtract = new  ExtractTextPlugin('styles.css');
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_API_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID ': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath:'/dist/'
+      publicPath: '/dist/'
     }
   };
 };
